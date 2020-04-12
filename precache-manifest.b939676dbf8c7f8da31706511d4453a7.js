c2e0b04c5cb282033b05ee54ad96705f13bb5472
KolosDan <dan.danya99@gmail.com>
2020-04-12 09:35:57 +0000

    Updates

--- precache-manifest.b939676dbf8c7f8da31706511d4453a7.js
+++ precache-manifest.b939676dbf8c7f8da31706511d4453a7.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/homerazrab3/static/js/runtime~main.5ce44b85.js"
   },
   {
-    "revision": "e2dff0f259c6e1841649",
-    "url": "/homerazrab3/static/js/main.b9720d2f.chunk.js"
+    "revision": "47ee617f2014c604951a",
+    "url": "/homerazrab3/static/js/main.bdbee876.chunk.js"
   },
   {
     "revision": "33dcd1393e95587f376f",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/homerazrab3/static/css/2.3a76fbd9.chunk.css"
   },
   {
-    "revision": "f55a68cb84dbbcd860961043903b7cd3",
+    "revision": "48fd57aac416f80a2ec5c8b062226d8e",
     "url": "/homerazrab3/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/homerazrab3/precache-manifest.af218cd25bb27f2f343b88bce6113617.js"
+  "/homerazrab3/precache-manifest.b939676dbf8c7f8da31706511d4453a7.js"
 );
 
 workbox.clientsClaim();
