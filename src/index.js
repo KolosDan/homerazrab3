e638f708850eb4b82126ddb7ea999664b69009bb
Your Name <you@example.com>
2020-04-12 15:16:49 +0500

    commit

--- src/index.js
+++ src/index.js
@@ -266,7 +266,7 @@ class Example extends React.Component {
   
       this.state = {
         activeStory : "discover",
-        sex : "",
+        sex : "male-straight",
         pref : [],
         age : 18,
         description : ""
@@ -350,7 +350,7 @@ class Example extends React.Component {
             </FormLayout>
 
             <FormLayout>
-              <Select name="sex" onChange={this.handleChange} top="Ваш гендер" placeholder="">
+              <Select value="male-straight" name="sex" onChange={this.handleChange} top="Ваш гендер" placeholder="">
                 <option value="male-straight">Мужчина Натурал</option>
                 <option value="female-straight">Женщина Натурал</option>
                 <option value="male-homo">Мужчина Гомосексуалист</option>
