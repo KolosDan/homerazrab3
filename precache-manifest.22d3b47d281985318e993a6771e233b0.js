2d855875b25952206b91e66cbf0339b2f459f607
Your Name <you@example.com>
2020-04-13 23:27:21 +0500

    Updates

---
+++ precache-manifest.22d3b47d281985318e993a6771e233b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c545212733ed2d18fdb",
+    "url": "/homerazrab3/static/js/main.7c0b4512.chunk.js"
+  },
+  {
+    "revision": "da546c1acf67f43793de",
+    "url": "/homerazrab3/static/js/runtime~main.5ce44b85.js"
+  },
+  {
+    "revision": "0e8dc36e9f6390fdecec",
+    "url": "/homerazrab3/static/css/2.a2cd734a.chunk.css"
+  },
+  {
+    "revision": "0e8dc36e9f6390fdecec",
+    "url": "/homerazrab3/static/js/2.ab17418b.chunk.js"
+  },
+  {
+    "revision": "a9af2035341ebec8e94eaa66c7fc1bb1",
+    "url": "/homerazrab3/index.html"
+  }
+];
