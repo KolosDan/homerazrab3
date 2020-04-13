a03373acc77dde4262b14209381e8ad890ddb1d2
KolosDan <dan.danya99@gmail.com>
2020-04-13 23:21:15 +0000

    removed token

--- notificator.py
+++ notificator.py
@@ -1,5 +1,6 @@
 import requests
 from pymongo import MongoClient
+import time
 
 def call_vk(method, access_token, **kwargs):
     arg_string = '&'.join(['%s=%s' % (k,v) for k,v in kwargs.items()])
@@ -12,15 +13,17 @@ def call_vk(method, access_token, **kwargs):
         raise Exception('ApiException', response['error']['error_msg'])
 
 db = MongoClient("mongodb://35.228.42.210:27017").dating
-token = '7fff76e57fff76e57fff76e5227f8f808777fff7fff76e5216bb1462ce9a8aa191e9040'
+token = 'TOKEN'
     
 while True:
-    notifications = [(i['from'], i['to']) for i in db.notifications.find()]
+    notifications = [(i['from'], i['to'], i['type']) for i in db.notifications.find()]
     try:
-        if set(old_notifications) - set(notifications) != 0:
-            for from_, to_ in set(old_notifications) - set(notifications):
+        if len(set(old_notifications) - set(notifications)) != 0:
+            for from_, to_, type_ in notifications:
+                print("Sending to %s" % str(to_))
                 if db.push.find_one({"user_id": to_}) != None:
                     call_vk("notifications.sendMessage", token, user_ids=str(to_), message="Новое уведомление от %s" % db.users.find_one({"user_id": from_})["first_name"])
     except Exception as e:
         print(e)
     old_notifications = notifications
+    time.sleep(3)
