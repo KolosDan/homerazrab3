a31b845167a62c35745aced5068ea2ac160fb175
Your Name <you@example.com>
2020-04-13 23:15:23 +0500

    Updates

--- precache-manifest.656e8984890f42eae36a9c49c8372eb8.js
+++ precache-manifest.656e8984890f42eae36a9c49c8372eb8.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "c465ca2ce4f96ff9c1fa",
-    "url": "/homerazrab3/static/js/main.2ce679ea.chunk.js"
+    "revision": "6578f4b386c3c5ed43fa",
+    "url": "/homerazrab3/static/js/main.f39daba8.chunk.js"
   },
   {
     "revision": "da546c1acf67f43793de",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/homerazrab3/static/js/2.b0157b34.chunk.js"
   },
   {
-    "revision": "6ec5483587e5c06815f2306a531c41bd",
+    "revision": "0008da8a7cfecc6020f1d447bb94efb5",
     "url": "/homerazrab3/index.html"
   }
 ];
